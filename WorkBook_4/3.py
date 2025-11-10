278afdb95dac6f1dc2d5879085c431c5e832eb32
Petrov Artem <artem_petrov_0407@mail.ru>
2025-11-10 11:26:52 +0300

    last commit

--- WorkBook_4/3.py
+++ WorkBook_4/3.py
@@ -1,11 +1,9 @@
 array = [5, 2, 4, 6, 7, 5, 1, 3, 2]
 for i in range(len(array)):
     current = array[i]
-    for j in range(len(array)):
+    for j in range(0, i):
         if current < array[j]:
             array.insert(j, current)
-            array.pop(i)
-            break
-        elif current == array[j]:
+            array.pop(i + 1)
             break
 print(array)
