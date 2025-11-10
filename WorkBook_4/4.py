d1d5cf5ffb8b45235f7a8b1dd0ca859ab204442b
Petrov Artem <artem_petrov_0407@mail.ru>
2025-11-10 20:09:27 +0300

    lst

--- WorkBook_4/4.py
+++ WorkBook_4/4.py
@@ -1,7 +1,9 @@
 array = [5, 2, 4, 6, 7, 5, 1, 3, 2]
-for i in range(len(array)):
+for i in range(len(array) - 1):
+    min_index = i
     for j in range(i + 1, len(array)):
-        if array[i] > array[j]:
-            array[i], array[j] = array[j], array[i]
-            
+        if array[min_index] > array[j]:
+            min_index = j
+    if min_index != i:
+        array[i], array[min_index] = array[min_index], array[i]         
 print(array)
