8197976de978ba2129f81a7e8e48610de71fd16a
Petrov Artem <artem_petrov_0407@mail.ru>
2025-11-22 20:34:31 +0300

    strart wb5

---
+++ WorkBook_5/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string line;
+    string newLine;
+    string current;
+    int left = 0;
+    getline(cin, line);
+    for (char(i): line)
+    {
+        newLine = i + newLine;
+    }
+    cout << newLine;
+}
