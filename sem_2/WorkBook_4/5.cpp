d1d3d35ad54607e28ed7831e83ff6e2894ef0046
penkkk <artem_petrov_0407@mail.ru>
2026-04-22 11:56:12 +0300

    lastcom

--- sem_2/WorkBook_4/5.cpp
+++ sem_2/WorkBook_4/5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 using namespace std;
 
 
@@ -18,6 +19,20 @@ struct Node{
 bool isRed(Node* node) { return node != nullptr && node->color == 'r'; }
 
 bool isBlack (Node* node) { return node == nullptr || node->color == 'b'; }
+
+Node* getGrandparent(Node* node){
+    if (node && node->parent->parent)
+        return node->parent->parent;
+    return nullptr;
+}
+
+
+Node* getUncle(Node* node){
+    Node* grandparent = getGrandparent(node);
+    if (!grandparent) return nullptr;
+    if (node->parent == grandparent->left) return grandparent->right;
+    else return grandparent->left;
+}
 
 
 void leftRotate(Node*& root, Node* oldMainNode) {
@@ -90,9 +105,49 @@ Node* insert(Node* root, int data){
         parent->right = newNode;
     }
 
-    
+    fixInsert(root, newNode);
 }
 
 
-void fixInsert()
+void fixInsert(Node*& root, Node* node){
+    while (node != root && isRed(node->parent)){
+
+        Node* parent = node->parent;
+        Node* uncle = getUncle(node);
+        Node* grandparent = getGrandparent(node);
+
+        if (isRed(uncle)) {
+            parent->color = 'b';
+            uncle->color = 'b';
+            grandparent->color ='r';
+            node = grandparent;
+        }
+        else{
+
+            if (parent = grandparent->left){
+                if (node == parent->right){
+                    leftRotate(root, parent);
+                    node = parent;
+                    parent = node->parent;
+                }
+                rotateRight(root, grandparent);
+                swap(parent->color, grandparent->color);
+                node = parent;
+            }
+            else{
+                if (node == parent->left){
+                    rotateRight(root, parent);
+                    node = parent;
+                    parent = node->parent;
+                }
+
+                leftRotate(root, grandparent);
+                swap(parent->color, grandparent->color);
+                node = parent;
+            }
+
+        }
+    }
+    root->color = 'b';
+}
 
